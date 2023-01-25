74bb66c06b4fe0b33b2deddd7467c53523453032
si-2032550093 <92351099+2032550093@users.noreply.github.com>
2023-01-25 10:34:42 +0700

    Add files via upload

---
+++ exe/cf.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+# Debian 9 & 10 x64
+# Ubuntu 18.04 & 20.04 x64
+# License forever-FREE-trial Autosript XRay by Du(^_^)De
+
+#Color
+red='\e[0;31m'
+lred='\e[1;31m'
+green='\e[0;32m'
+lgreen='\e[1;32m'
+gold='\e[0;33m'
+yellow='\e[1;33m'
+blue='\e[0;34m'
+lblue='\e[1;34m'
+purple='\e[0;35m'
+lpurple='\e[1;35m'
+cyan='\e[0;36m'
+lcyan='\e[1;36m'
+NC='\e[0m'
+
+#preparation
+rm domain
+rm nsdomain
+rm -f /root/domain
+rm -f /root/nsdomain
+rm -f /etc/xray/domain
+rm -f /etc/v2ray/domain
+rm -f /var/lib/crot/ipvps.conf
+mkdir -p /usr/bin/xray
+mkdir -p /usr/bin/v2ray
+mkdir -p /etc/xray
+mkdir -p /etc/v2ray
+
+#install curl
+apt install jq curl -y
+
+#setup domain
+sub=$(date +%Y%m%d-%H%M%S)
+DOMAIN=dude.my.id
+NS_DOMAIN=githubvpn-${sub}.xray.dude.my.id
+SUB_DOMAIN=${sub}.xray.dude.my.id
+CF_ID=sc.dude.my.id@gmail.com
+CF_KEY=e4e896fae1987132b12386f9b399d5d333a64
+
+set -euo pipefail
+IP=$(wget -qO- icanhazip.com);
+echo "Updating DNS for ${SUB_DOMAIN}..."
+ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
+
+RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${SUB_DOMAIN}" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
+
+if [[ "${#RECORD}" -le 10 ]]; then
+     RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}' | jq -r .result.id)
+fi
+
+RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}')
+echo "Updating DNS NS for ${NS_DOMAIN}..."
+ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
+
+RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${NS_DOMAIN}" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
+
+if [[ "${#RECORD}" -le 10 ]]; then
+     RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"NS","name":"'${NS_DOMAIN}'","content":"'${SUB_DOMAIN}'","ttl":120,"proxied":false}' | jq -r .result.id)
+fi
+
+RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"NS","name":"'${NS_DOMAIN}'","content":"'${SUB_DOMAIN}'","ttl":120,"proxied":false}')
+
+rm -rf /etc/xray/domain
+rm -rf /root/nsdomain
+echo "IP=""$SUB_DOMAIN" >> /var/lib/crot/ipvps.conf
+echo "Host : $SUB_DOMAIN"
+echo $SUB_DOMAIN > /root/domain
+echo "Host SlowDNS : $NS_DOMAIN"
+echo "$NS_DOMAIN" >> /root/nsdomain
+echo "$SUB_DOMAIN" >> /etc/xray/domain
+echo "$SUB_DOMAIN" >> /etc/v2ray/domain
+sleep 3
+cd
